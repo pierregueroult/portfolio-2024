745c2a32810000d0c6ee45ee6ec61c54fa44a858
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2023-10-13 10:19:12 +0200

    Ajout du style de base et de la typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
 import "./globals.scss";
 import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { NextFontWithVariable } from "next/dist/compiled/@next/font";
+
+const font: NextFontWithVariable = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font",
+});
 
 export const metadata: Metadata = {
   title: "Portfolio 2024 - Pierre Guéroult 🪶",
   description: "",
 };
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="fr" dir="ltr">
+    <html lang="fr" dir="ltr" className={font.variable}>
       <body>{children}</body>
     </html>
   );
