44b873dd608356d16b56a2f5c0506ca126395163
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2023-11-02 15:18:14 +0100

    Encore des fix à droite à gauche

--- src/app/api/gallery/route.ts
+++ src/app/api/gallery/route.ts
@@ -1,15 +1,19 @@
 import getImagesData from "@/contents/getImagesData";
+import { Photo } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  var imagesData = await getImagesData();
   const searchParams: URLSearchParams = req.nextUrl.searchParams;
   const start: string | null = searchParams.get("start");
   const end: string | null = searchParams.get("end");
 
-  if (start && end) {
-    imagesData = imagesData.slice(Number(start), Number(end));
-  }
+  if (!start || !end) return NextResponse.error();
+
+  const images: Photo[] | null = await getImagesData();
+
+  if (!images) return NextResponse.error();
+
+  const imagesData: Photo[] = images.slice(Number(start), Number(end));
 
   return NextResponse.json(imagesData);
 }
