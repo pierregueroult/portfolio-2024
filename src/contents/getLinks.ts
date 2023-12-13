fc2862f05a3e58517e537b2fa6bad218147970de
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2023-12-13 18:40:28 +0100

    Mise à jour du CV et des liens

--- src/contents/getLinks.ts
+++ src/contents/getLinks.ts
@@ -9,6 +9,7 @@ export default function getLinks() {
   const links: linksType = [
     { name: "Accueil", href: "/" },
     { name: "Projets", href: "/projects" },
+    { name: "CV", href: "/curriculum-vitae" },
     // { name: "Blog", href: "/blog" },
     // { name: "Gallerie", href: "/gallery" },
   ];
